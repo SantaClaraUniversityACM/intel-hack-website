1d21070b1b83c0f8203e199fbbb4e90eff37d03a
purple-affogato <ivy.yiting.zhuang@gmail.com>
2025-06-08 10:28:15 -0700

    Forgot to update timer whoops

--- js/timer.js
+++ js/timer.js
@@ -5,19 +5,22 @@ const header = document.querySelector("h2.timerHeader");
 
 function updateCountdown() {
     const countdown = new Date("2025-05-17T16:00:00Z").getTime() - new Date().getTime();
-    if (countdown < 0) {
-        const duration = new Date("2025-05-18T03:00:00Z").getTime() - new Date().getTime();
-        if (duration < 0) {
-            header.innerHTML = "Judging in session!";
-            const judgingTime = new Date("2025-05-24T19:00:00Z").getTime() - new Date().getTime();
-            timer.innerHTML = generateTime(Number(judgingTime));
-        }
-        else {
-            header.innerHTML = "Hackathon in session!";
-            timer.innerHTML = generateTime(Number(duration));
-        }
+    const duration = new Date("2025-05-18T03:00:00Z").getTime() - new Date().getTime();
+    const judgingTime = new Date("2025-05-24T19:00:00Z").getTime() - new Date().getTime();
+
+    if (judgingTime < 0) {
+        header.innerHTML = "Intel Hack is over! Thank you to all who attended!";
+    }
+    else if (duration < 0) {
+        header.innerHTML = "Judging in session!";    
+        timer.innerHTML = generateTime(Number(judgingTime));
+    }
+    else if (countdown < 0) {
+        header.innerHTML = "Hackathon in session!";
+        timer.innerHTML = generateTime(Number(duration));
     }
     else {
+        header.innerHTML = "Time till Intel Hack!";
         timer.innerHTML = generateTime(Number(countdown));
     }
 }
